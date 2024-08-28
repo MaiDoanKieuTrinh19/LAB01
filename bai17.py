ec542cd112b248f76930afef057a696dd397f4f8
MaiDoanKieuTrinh19 <maidoankieutrinh@gmail.com>
2024-08-28 19:50:46 +0700

    Add files via upload

---
+++ bai17.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 19:43:23 2024
+
+@author: MaiDoanKieuTrinh_23732851
+"""
+a=int(input('Nhập số nguyên thứ nhất: '))
+b=int(input('Nhập số nguyên thứ hai: '))
+c=int(input('Nhập số nguyên thứ ba: '))
+solonnhat=max(a,b,c)
+print(f'Số lớn nhất trong ba số nguyên {a}, {b}, {c} là: {solonnhat}')
+sonhonhat=min(a,b,c)
+print(f'Số nhỏ nhất trong ba số nguyên {a}, {b}, {c} là: {sonhonhat}')
+
